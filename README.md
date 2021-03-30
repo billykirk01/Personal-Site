9bbd0b64c6b5ca4715f7a99ec9e1f36c7a303675
Billy Kirk <kirk.william.c@gmail.com>
2021-03-30 12:26:01 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Personal website
+
+[billykirk.dev](billykirk.dev)
+
+Built with [Svelte](https://svelte.dev/) and hosted on [Firebase](https://firebase.google.com/).
---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  let name = "Billy";
+</script>
+
+<main>
+  <h1>Hi, my name is {name}!</h1>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    background-color: #f7f8fa;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 2rem auto;
+    max-width: 14rem;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
+  }
+</style>
